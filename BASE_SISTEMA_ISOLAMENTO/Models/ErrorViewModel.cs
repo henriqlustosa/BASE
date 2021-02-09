02a1e21237ac961426e9f2dafb8e2b40cf1cce3d
Henrique Lustosa <henriqlustosa@gmail.com>
2021-02-09 14:50:33 -0300

    Add project files.

---
+++ BASE_SISTEMA_ISOLAMENTO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BASE_SISTEMA_ISOLAMENTO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
